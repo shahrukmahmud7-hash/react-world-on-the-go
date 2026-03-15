e3ef08f456b1e5ecb21c504216e9f5889a181ca8
shahrukmahmud7-hash <shahrukmahmud7@gmail.com>
2026-03-15 18:56:34 +0600

    first commit

---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+          <h2>In The Countries.....</h2>  
+        </div>
+    );
+};
+
+export default Countries;
